746352381da7dd710a403f7100db3523bfeaa096
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-08-10 17:33:51 +0530

    created upcoming features section

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <main className="container mx-auto px-4 py-16 relative z-10">
           <div className="flex flex-col md:flex-row items-center justify-between">
             <div className="md:w-1/2 mb-8 md:mb-0">
-              <h2 className="text-4xl md:text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Experience Solnext</h2>
+              <h2 className="text-4xl md:text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Experience Solnext Currently live on devnet</h2>
               <p className="text-xl mb-8">Securely power your solana experience with solnext mobile wallet.</p>
               <button className="bg-white text-purple-600 px-6 py-3 rounded-full font-semibold flex items-center hover:bg-purple-100 transition duration-300">
                 Get Started <ArrowRight className="ml-2" />
@@ -36,9 +36,9 @@ export default function Home() {
         </main>
       </div>
 
-      <section id="features" className="py-16 bg-white">
+      <section id="features" className="py-16 bg-[#111111]">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 text-center text-black">Solnext Features</h2>
+          <h2 className="text-3xl font-bold mb-8 text-center text-white">Solnext Features</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <FeatureCard feature='Intuitive Interface' description='Receive and Send SOL/USDC in an interactive manner to any wallet.'></FeatureCard>
             <FeatureCard feature='Proudly Open Source 💚' description='Solnext is a cross platform mobile app built with Flutter framework and is proudly open source.'></FeatureCard>
@@ -47,7 +47,19 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="download" className="py-16 bg-black">
+      <section id="upcoming" className="py-16 bg-[#111111]">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 text-center text-white">Upcoming Features</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <FeatureCard feature='Solnext DEX' description='In future users will have the option to swap SOL/USDC using Solnext Decentralized Exchange.'></FeatureCard>
+          <FeatureCard feature='DCA' description='Dollar cost averaging will allow users to automatically trade any SPL token at regular interval over a period of time.'></FeatureCard>
+          <FeatureCard feature='Limit Orders' description='With limit orders, you will have the flexibility to trade any SPL token in your specified price limit.'></FeatureCard>
+            
+          </div>
+        </div>
+      </section>
+
+      <section id="download" className="py-16 bg-[#111111]">
         <div className="container mx-auto px-4 text-center">
           <h2 className="text-3xl font-bold mb-8">Download SolNext Now</h2>
           <div className="flex justify-center space-x-4">
@@ -61,7 +73,7 @@ export default function Home() {
         </div>
       </section>
 
-      <footer id="contact" className="py-8 bg-purple-900">
+      <footer id="contact" className="py-8 bg-[#111111]">
         <div className="container mx-auto px-4 text-center">
           <p>&copy; 2024 SolNext App. All rights reserved.</p>
           <div className="mt-4">
--- components/FeatureCard.tsx
+++ components/FeatureCard.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const FeatureCard = ({ feature, description }: any) => {
   return (
-    <div className="bg-black p-6 rounded-lg ">
-      <h3 className="text-xl font-semibold mb-2">{feature}</h3>
-      <p>{description}</p>
+    <div className="bg-white p-6 rounded-lg ">
+      <h3 className="text-xl font-semibold mb-2 text-black">{feature}</h3>
+      <p className='text-black'>{description}</p>
     </div>
   );
 };
